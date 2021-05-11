f1dfb014628b6797ae8b178b16409f33b86fd6d1
Yann Braga <yannbf@gmail.com>
2021-05-11 21:40:24 +0200

    cleanup basic template

--- src/pages/Content/modules/print.js
+++ src/pages/Content/modules/print.js
@@ -1,3 +1,4 @@
+const styles = ['%cMyExtension', 'color: white;background:#0401a4;font-weight: bold; font-size:10px; padding:2px 6px; border-radius: 5px']
 export const printLine = (line) => {
-  console.log('===> FROM THE PRINT MODULE:', line);
+  console.log(...styles, line);
 };
--- src/pages/Devtools/index.js
+++ src/pages/Devtools/index.js
@@ -1,5 +1,5 @@
 chrome.devtools.panels.create(
-  'Dev Tools from chrome-extension-boilerplate-react',
+  'My Custom Dev tool',
   'icon-34.png',
   'panel.html'
 );
